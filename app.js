17a98548e249e30603b6f54b49a1a5096516cfc6
Anil khand <anilkhandthakuri@gmail.com>
2026-02-04 23:36:07 +1100

    Update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-const allowedOrigin = "https://thakurianil-trackmyspend-j5ho0d9xc-anil-khands-projects.vercel.app" || "http://localhost:3000";
+const allowedOrigin = "https://thakurianil-trackmyspend.vercel.app" || "http://localhost:3000";
 
 app.use(
     cors({
